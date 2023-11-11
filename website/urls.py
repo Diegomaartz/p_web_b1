8a74eb0856801645c2eb9f3f66b045653c230d14
Mendez <diegomartinez13272@gmail.com>
2023-11-11 15:52:57 -0600

    registration

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name="home"),
     # path('login/', views.login_user, name='login'),
-    # path('logut/', views.logout_user, name='logout'),
+    path('logut/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
 
